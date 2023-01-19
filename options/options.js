040be00ca9837beb97672133ee355daeac868737
Crowdbotics <team@crowdbotics.com>
2023-01-19 09:30:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "xolamobileapp-38358",
+  url: "https://xolamobileapp-38358.botics.co",
+  api: "https://xolamobileapp-38358.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
